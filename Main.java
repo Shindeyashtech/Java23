eeada389d20daa88ae9c89f1a22a54d35cb7ff64
Yash shinde <yashshinde.tech@gmail.com>
2024-09-21 06:45:55 +0000

    pattern matching before23

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-System.out.println("Hello, World!");
+int x = 5;
+switch (x) {
+    case 1, 2, 3 -> System.out.println("Low");
+    case 4, 5, 6 -> System.out.println("Medium");
+    case 7, 8, 9 -> System.out.println("High");
+    default -> System.out.println("Unknown");
+}
